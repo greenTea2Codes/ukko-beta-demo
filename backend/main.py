1f7102c5b6f288d196d788c03c613f3d8012a4cd
dc <dalunchen.fi@gmail.com>
2025-03-31 13:13:59 +0300

    feat: allow CORS from the front-end app

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,27 @@
+from dotenv import load_dotenv
+import os
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from llm import get_llm_response
 from weather import get_weather
 
 app = FastAPI()
+
+front_end_url=os.getenv("FRONT_END_URL")
+origins = [
+    front_end_url
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class UserInput(BaseModel):
     message: str
